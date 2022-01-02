6e84c24601eff98121a0ee187b0c781c2c735e88
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2022-01-02 22:34:05 +0700

    2022

---
+++ README.md
@@ -0,0 +1,16 @@
+# 2022 | 📷 from Long Nhat Nguyen
+
+Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Using the Gatsby Theme [`@lekoarts/gatsby-theme-emilia`](https://github.com/LekoArts/gatsby-themes/tree/main/themes/gatsby-theme-emilia).
+
+## Installation
+
+```sh
+npm install
+npm run develop
+```
+
+The site is now running at `http://localhost:8000`!
+
+## License
+
+Licensed under the [CC0-1.0](LICENSE) license.
---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: "/2022",
+  siteMetadata: {
+    // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-emilia-core/gatsby-config.js
+    siteTitle: `2022`,
+    siteTitleAlt: `2022 | 📷 from Long Nhat Nguyen`,
+    siteHeadline: `2022 | 📷 from Long Nhat Nguyen`,
+    siteUrl: `https://retro90s.github.io/2022/`,
+    siteDescription: "📷 from Long Nhat Nguyen in 2022",
+    siteLanguage: `en`,
+    author: `@torn4dom4n`,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-emilia`,
+      options: {
+        name: `2022`,
+        socialMedia: [{}],
+        location: `Vietnam`,
+      },
+    },
+  ],
+}
